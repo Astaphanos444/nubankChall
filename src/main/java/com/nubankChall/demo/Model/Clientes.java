564d3eda0474756886407714abf2ff1798b50cf9
chris <christianpimpo304@hotmail.com>
2025-04-20 20:21:30 -0300

    model

---
+++ src/main/java/com/nubankChall/demo/Model/Clientes.java
@@ -0,0 +1,34 @@
+package com.nubankChall.demo.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Clientes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany( mappedBy = "clientes", 
+                cascade = CascadeType.ALL, 
+                orphanRemoval = true )
+    private List<Object> contatos = new ArrayList<Object>();
+}
---
+++ src/main/java/com/nubankChall/demo/Model/Contato.java
@@ -0,0 +1,27 @@
+package com.nubankChall.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String telefone;
+    private String email;
+    
+    @ManyToOne
+    @JoinColumn(name="cliente_id")
+    private Clientes cliente;
+
+    
+}
